9f4171cd1f30d2ba4f187c32d956cf9ca136269c
Luca Pengelly <xophnex@protonmail.ch>
2021-04-03 19:56:48 +0100

    New scene for the actual simulation with stub UDP receiver code

---
+++ frontend/AI Racing Game/Assets/car.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For multithreading
+using System.Threading;
+
+// Sockets stuff
+using System.Net.Sockets;
+using System.Net;
+
+public class car : MonoBehaviour
+{
+	Thread receiverThread;
+	UdpClient client;
+
+	public int port = 59827; // picked randomly by Luca from a port range outside of those that can be registered with IANA
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		receiverThread = new Thread(new ThreadStart(ReceiveData));
+		receiverThread.IsBackground = true;
+		receiverThread.Start();
+
+		Debug.Log("starting receiverThread");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	// Function for the thread, listens for packets from the server
+	private void ReceiveData()
+	{
+		//try {
+			client = new UdpClient(port);
+			while (Thread.CurrentThread.IsAlive) {
+				
+			}
+		//} catch (exception e) { }
+	}
+
+	public void OnApplicationQuit()
+	{
+		if (receiverThread != null) {
+			receiverThread.Abort();
+		}
+
+		Debug.Log("aborting receiverThread");
+	}
+}
