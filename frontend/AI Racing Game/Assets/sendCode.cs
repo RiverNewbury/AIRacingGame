e392482371e85e5e94fd2ad870605fad1ceae5b3
Luca Pengelly <xophnex@protonmail.ch>
2021-04-05 18:54:58 +0100

    Basic HTTP POST to send user's code

--- frontend/AI Racing Game/Assets/sendCode.cs
+++ frontend/AI Racing Game/Assets/sendCode.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 
@@ -9,6 +10,7 @@ public class sendCode : MonoBehaviour
 {
 	public InputField codeField;
 	public InputField usernameField;
+	public String serverAddress;
 
 
 	// Start is called before the first frame update
@@ -25,10 +27,19 @@ public class sendCode : MonoBehaviour
 
 	public void SendCode()
 	{
-		// Dummy code that just tests it
-		Debug.Log(codeField.text);
-		Debug.Log(usernameField.text);
+		// Modified from example code at docs.unity3d.com/Manual/UnityWebRequest-SendingForm.html
+		WWWForm form = new WWWForm();
+		form.AddField("username", usernameField.text);
+		form.AddField("code", codeField.text);
 
-		// Need network code to send it off to the server here
+		UnityWebRequest www = UnityWebRequest.Post(serverAddress, form);
+		yield return www.SendWebRequest();
+
+		if (www.result != UnityWebRequest.Result.Success) {
+			Debug.Log(www.error);
+		}
+		else {
+			Debug.Log("Form upload complete!");
+		}
 	}
 }
