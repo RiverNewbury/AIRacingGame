26f416ab416254800127938052fd771a1fad38c3
Luca Pengelly <xophnex@protonmail.ch>
2021-04-05 21:55:32 +0100

    Added very basic HTTP GET code to the simulation scene

--- frontend/AI Racing Game/Assets/carHTTP.cs
+++ frontend/AI Racing Game/Assets/carHTTP.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Networking;
+
 public class carHTTP : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public string serverAddress;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Get request
+		UnityWebRequest webRequest = UnityWebRequest.Get(serverAddress);
+
+		if (webRequest.result != UnityWebRequest.Result.Success) {
+			Debug.Log(webRequest.error);
+		} else {
+			// Show results as text
+			Debug.Log(webRequest.downloadHandler.text);
+
+			// Or retrieve results as binary data
+			byte[] results = webRequest.downloadHandler.data;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
