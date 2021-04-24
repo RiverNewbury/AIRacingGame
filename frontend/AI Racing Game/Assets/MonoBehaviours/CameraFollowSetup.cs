d3a1d78dfbe9bc7d075f93a4d68b6780a24ab013
b-grant <57621440+b-grant@users.noreply.github.com>
2021-04-24 21:15:18 +0100

    Leader board
    Think this works, would be good to test on someone else's computer

---
+++ frontend/AI Racing Game/Assets/MonoBehaviours/CameraFollowSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AIRacing.MonoBehaviours {
+
+    public class CameraFollowSetup : MonoBehaviour {
+
+        [SerializeField] private CameraFollow cameraFollow;
+        [SerializeField] private Transform followTransform;
+        [SerializeField] private float zoom;
+
+        private void Start() {
+            if (followTransform == null) {
+                Debug.LogError("followTransform is null! Intended?");
+                cameraFollow.Setup(() => Vector3.zero, () => zoom);
+            } else {
+                cameraFollow.Setup(() => followTransform.position, () => zoom);
+            }
+        }
+    }
+
+}
