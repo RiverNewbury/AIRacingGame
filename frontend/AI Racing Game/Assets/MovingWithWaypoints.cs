86d01c28af7f6a07b47bc6400d8918dc42d0f5df
b-grant <57621440+b-grant@users.noreply.github.com>
2021-04-26 17:46:56 +0100

    Fixed bugs in code
    This doesn't have network mesh in it yet, adding that now. This is just to document fixes

--- frontend/AI Racing Game/Assets/MovingWithWaypoints.cs
+++ frontend/AI Racing Game/Assets/MovingWithWaypoints.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AIRacing.Utils;
 
 public class MovingWithWaypoints : MonoBehaviour
 {
@@ -17,24 +18,74 @@ public class MovingWithWaypoints : MonoBehaviour
         public int time;
     }
 
+    //public static history = InfoObject.history
+
     public static float[][] sentwps = 
     {
         new[] {0.0f,0.0f},
-        new[] {0.5f,0.5f},
-        new[] {1.0f,1.0f}
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-5.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,0.0f},
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-5.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,0.0f},
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-5.0f},
+        new[] {0.0f,-4.0f},
+        new[] {0.0f,-3.0f},
+        new[] {0.0f,-2.0f},
+        new[] {0.0f,-1.0f},
+        new[] {0.0f,0.0f},
+        new[] {1.0f,0.0f},
+        new[] {2.0f,0.0f},
+        new[] {3.0f,0.0f},
+        new[] {4.0f,0.0f},
+        new[] {5.0f,0.0f},
+        new[] {6.0f,0.0f},
+        new[] {7.0f,0.0f},
+        new[] {8.0f,0.0f},
+        new[] {9.0f,0.0f},
+        new[] {10.0f,0.0f},
+        new[] {9.0f,0.0f},
+        new[] {8.0f,0.0f},
+        new[] {7.0f,0.0f},
+        new[] {6.0f,0.0f},
+        new[] {5.0f,0.0f},
+        new[] {4.0f,0.0f},
+        new[] {3.0f,0.0f},
+        new[] {2.0f,0.0f},
+        new[] {1.0f,0.0f},
+        new[] {0.0f,0.0f},
     };
-    public static float[] speed = {1.0f, 5.0f, 12.0f};
+    //public static float[] speed = {1.0f, 5.0f, 12.0f};
     //might need to alter speed
-    public int tps = 100;
+    public float tps = 1;
+    public float spt = 1f;
     public Score score = new Score {
         success = true,
-        time = 120}
-    ;
+        time = 120};
     public static int swpl = sentwps.Length;
-    public Vector2[] waypoints = new Vector2[swpl];
+    //public Vector2[] waypoints = new Vector2[swpl];
     public float WPRadius = 0.01f;
     public int current = 0;
-    public bool crashed = false;
+    public bool crashed = true;
 
     private static float[] ConvertPos(float[] pos, Vector2 dif)
         {
@@ -42,12 +93,13 @@ public class MovingWithWaypoints : MonoBehaviour
         float ydif = dif.y;
         float x = pos[0];
         float y = pos[1];
-        float SW = 10; //guess for now
-        float CW = 10; //ditto
+        float SW = 2; //guess for now
+        float CW = 0.125f; //ditto
         float SH = 10; //guess for now
-        float CH = 10; //ditto
+        float CH = 4; //ditto
+        
         float wx = (CW * x)/SW + xdif;
-        float wy = (CH* x)/SH + ydif;
+        float wy = (CH* y)/SH + ydif;
         float[] newpos = new float[] {wx,wy}; 
         return newpos;
     }
@@ -59,23 +111,32 @@ public class MovingWithWaypoints : MonoBehaviour
         //calculate waypoints from history, so set start to current pos etc.
         Vector2 currentPosition = this.transform.position;
         var i = 0;
+        Debug.Log(swpl);
         //Vector2[] _Waypoints = new Vector2[history.Length];
         while (i<swpl){
             sentwps[i] = ConvertPos(sentwps[i], currentPosition);
-            waypoints[i] = new Vector2(waypoints[i][0], waypoints[i][1]);
+            //waypoints[i] = new Vector2(sentwps[i][0], sentwps[i][1]);
             i++;
         }
+        //Debug.Log(waypoints);
+        //i = 0
         //position = waypoints[current].transform.position; 
+        //InvokeRepeat("ChangePosition",0,spt)
     }
+
+    //void ChangePosition () {
+    //    transform.position = waypoints[i];
+    //    i+=1;
+    //}
 
     // Update is called once per frame
     void Update()
     {
         current++;
-        if (current >= waypoints.Length)
+        if (current >= swpl)
         {
                 if (crashed){
-                    //animation for crash
+                    entryTransform.Find("circle").GetComponent<Image>().color = UtilsClass.GetColorFromString("B76F56");
                 }
                 else{
                     //show time
@@ -87,7 +148,10 @@ public class MovingWithWaypoints : MonoBehaviour
         else {
             //if(Vector2.Distance(waypoints[current].transform.position, transform.position) < WPRadius)
             //{
-                transform.position = Vector2.MoveTowards(transform.position, waypoints[current], Time.deltaTime*speed[current]);
+                Debug.Log(current);
+                var newP = new Vector2(sentwps[current][0],sentwps[current][1]);
+                transform.position = Vector2.MoveTowards(transform.position, newP, Time.deltaTime * tps);
+                //current+=1;
             //}
             if ((current % tps) == 0)
             {
