f01d9ae08ad1528f8500fd6a262e6651951c2a43
sharnoff <github@max.sharnoff.org>
2021-04-27 02:59:49 -0700

    ensure 'run-server-code.sh' always kills the server

--- server/run-server-code.sh
+++ server/run-server-code.sh
@@ -10,11 +10,10 @@ cargo build
 target/debug/ai-racing-server &
 
 server_pid=$!
+trap "kill $server_pid" EXIT
 
 # Because the server sometimes takes a while to start up, we'll sleep for a bit
 # to give it a chance.
 sleep 0.5
 
 curl -X POST --data-binary '@test.py' "http://localhost:8000/run/test_user"
-
-kill $server_pid
