836d0b76b6848e7387acbe53b99ba70f9fa1074f
RiverNewbury <pookibrine@gmail.com>
2021-04-27 13:01:29 +0100

    Tried to get a bit better test code but it currently goes in a circle forever

--- server/test.py
+++ server/test.py
@@ -15,5 +15,18 @@ def outputs(env):
             best_i = i
             max_d = d
 
+    rad_per = 2*3.1415 / len(env.dist_to_wall)
+
+    desired_angle = best_i*rad_per
+
+    current_angle = env.car_currently.angle
+
+    if ((desired_angle-current_angle) > 3.1415/4):
+        turn = 0
+    elif (desired_angle < current_angle):
+         turn = -0.2
+    else:
+         turn = 0.2
+
     dist_len = len(env.dist_to_wall)
-    return CarCommand(acc = 1.0, turning_speed = 2 * (i / (dist_len - 1)) - 1)
+    return CarCommand(acc = 1, turning_speed = turn)
